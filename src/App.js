57f3a7823e4c7cf594898ffc5b06832ecee45c7a
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2023-01-13 10:18:50 +0300

    added fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginPage from "./pages/loginPage/LoginPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
